29df4b4908373c42279ebcf4d5b600eb4ac4bac8
ktsvilii <kos.tsvilii@gmail.com>
2024-07-27 18:31:41 +0300

    Adding user controller
    Established connection with cloudinary
    Created notification model

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 import express from 'express';
-
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
+
+import { v2 as cloudinary } from 'cloudinary';
+
+import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/user.routes.js';
 
 import connect from './db/connect.js';
 
-import authRoutes from './routes/auth.routes.js';
-import cookieParser from 'cookie-parser';
-
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.config.CLOUDINARY_CLOUD_NAME,
+  api_key: process.config.CLOUDINARY_API_KEY,
+  api_secret: process.config.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 
@@ -16,8 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
-
-console.log(process.env.MONGO_URI);
+app.use('/api/users', userRoutes);
 
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
